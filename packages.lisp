5c224985cf68805bc5333b71fc50dc7261917c9a
Matthias Hoelzl <tc@xantira.com>
2012-12-21 15:07:21 +0100

    Fixed copyright headers

--- packages.lisp
+++ packages.lisp
@@ -1,6 +1,7 @@
 ;;; -*- Mode: Lisp; common-lisp-style: poem -*-
 
 ;;; Copyright (c) 2012 Matthias Hölzl
+;;; Copyright (c) 2012 Lenz Belzner
 ;;; 
 ;;; This file is licensed under the MIT license; see the file LICENSE
 ;;; in the root directory for further information.
